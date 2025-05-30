71aa56d850d4d044046932ac1609f16e1978974c
Congglee <congldqn888@gmail.com>
2025-05-31 01:05:55 +0700

    Implement Topbar component with theme toggle functionality and add theme configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Topbar from '~/components/Topbar'
+
 function App() {
-  return <div className='app'>Hello World</div>
+  return (
+    <div className='app'>
+      <main className='content'>
+        <Topbar />
+      </main>
+    </div>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
+import CssBaseline from '@mui/material/CssBaseline'
+import GlobalStyles from '@mui/material/GlobalStyles'
+import { CssVarsProvider } from '@mui/material/styles'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from '~/App'
 import '~/index.css'
+import theme from '~/theme'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <CssVarsProvider theme={theme}>
+        <GlobalStyles styles={{ a: { textDecoration: 'none' } }} />
+        <CssBaseline />
+        <App />
+      </CssVarsProvider>
     </BrowserRouter>
   </StrictMode>
 )
